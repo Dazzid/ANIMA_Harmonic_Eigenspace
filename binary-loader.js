62cd9d167a67be790f2699457c715bcbab5094cf
Dazzid <davmazo@gmail.com>
2025-11-03 18:49:07 +0100

    new version

--- binary-loader.js
+++ binary-loader.js
@@ -95,7 +95,7 @@ async function loadDissonanceMap(baseFreq = 220, nodes = 400, onProgress) {
                         reachedTarget = true;
                     }
                     if (typeof onProgress === 'function') {
-                        const percent = Math.min(100, Math.floor((loadedChunks + 1) * 100));
+                        const percent = Math.min(100, Math.floor((loadedChunks) * 100));
                         onProgress(percent, `Loading dataset… (${loadedChunks}/${TOTAL_CHUNKS})`);
                     }
                     break;
